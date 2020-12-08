554c6d92524552b27d050a142a331cf4eb822df8
Niels <n.a.de.jong@student.rug.nl>
2020-12-08 21:40:05 +0100

    Prepare a first version of preprocessing
    The only dimension reduction tool we currently have is linear PCA.
    Of course, scaling and shifting is included as well. Additionally, a
    convenience method is introduced to search for 'knees' in the
    eigenvalues of the PCA's SVD.

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 import math
 import copy as cp
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import random as rd
+from sklearn.decomposition import PCA
 from typing import List, Optional, Tuple
 
 
@@ -35,19 +37,18 @@ class RecordSet:
 		return ra, rb
 
 	def center(self, also_categorical: bool = False) -> None:
-		for col in range(len(self.entries.shape[1])):
+		for col in range(self.entries.shape[1]):
 			if self.types[col] == np.dtype('bool') and not also_categorical:
 				continue
 			self.entries[:, col] -= self.entries[:, col].mean()
 
 	def scale(self, also_categorical: bool = False) -> None:
-		for col in range(len(self.entries.shape[1])):
+		for col in range(self.entries.shape[1]):
 			if self.types[col] == np.dtype('bool') and not also_categorical:
 				continue
 			self.entries[:, col] /= self.entries[:, col].var()
 
 	def normalise(self, also_categorical: bool = False) -> None:
-		# TODO: Important. Perhaps we first need to use PCA, and only then standardise.
 		self.center(also_categorical)
 		self.scale(also_categorical)
 
@@ -60,5 +61,38 @@ def raw_data(location: str) -> RecordSet:
 		raise Exception('Could not find the file \'' + location + '\'.')
 
 
+def apply_pca(r: RecordSet, dim: Optional[int]) -> PCA:
+	inp: np.ndarray = r.entries[:, :(r.entries.shape[1] - 1)]
+	out: np.ndarray = r.entries[:, (r.entries.shape[1] - 1):]
+	pca: PCA = PCA(n_components=dim)
+	pca.fit(inp)
+	if dim is None:
+		dim = pca.explained_variance_.shape[0]
+	r.entries = np.concatenate((pca.transform(inp), out), axis=1)
+	r.names = ['component_' + str(i + 1) for i in range(dim)] + [r.names[len(r.names) - 1]]
+	r.types = [np.dtype('float64') for i in range(dim)] + [r.types[len(r.types) - 1]]
+	return pca
+
+
+def visualise_pca_components(pca: PCA, block: bool = True) -> None:
+	# gather the bar values
+	n: int = pca.explained_variance_.shape[0]
+	x: List[int] = list(range(n))
+	y: np.ndarray = pca.explained_variance_
+	# make the actual plot
+	plt.bar(x, y, color='black')
+	plt.xticks(list(range(n)))
+	plt.xlabel('PCA component')
+	plt.ylabel('Variance accounted for (VAF)')
+	plt.title('VAF per PCA Component Used')
+	plt.tight_layout()
+	plt.show(block=block)
+
+
 if __name__ == '__main__':
 	rec: RecordSet = raw_data('data.csv')
+	gen: rd.Random = rd.Random(123)  # for reproducibility
+	tv, te = rec.partition(0.7, gen)  # two datasets, namely train-validate and test
+	tv.normalise()
+	pca = apply_pca(tv, None)
+	visualise_pca_components(pca, block=True)
