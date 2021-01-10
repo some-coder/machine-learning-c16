fdb2526334e60e82163a2c0f10b0345689827c1e
aludi <ludivanleeuwen@gmail.com>
2021-01-10 16:16:03 +0100

    added priors to naive bayes

--- learners/naive_bayes.py
+++ learners/naive_bayes.py
@@ -14,39 +14,59 @@ class NaiveBayes(Learner):
 	data-points.
 	"""
 
-	def __init__(self, **params: any) -> None:
+	def __init__(self, prior, **params: any) -> None:
 		"""
-		Constructs a base learning algorithm.
+        Constructs a base learning algorithm.
 
-		:param params: Parameter values to supply.
-		"""
+         :param params: Parameter values to supply.
+        """
 		super().__init__(**params)
-		self.name = 'Naive Bayes Classifier'
 
 		self.data: Optional[RecordSet] = None
 		self.bayes_model = None
+		# prior = prior probability of class = 0
+
+		self.prior = prior
 
 	def fit(self, rs: RecordSet) -> None:
 		"""
-		Fits the base learning algorithm to training data.
+        Fits the base learning algorithm to training data.
 
-		:param rs: The record set to provide as training input.
-		"""
+        :param rs: The record set to provide as training input.
+        """
 		self.data = cp.deepcopy(rs)
 		x = self.data.entries[:, :-1]
 		y = np.ravel(self.data.entries[:, -1:])
-		bayes_classifier = GaussianNB()
+		if self.prior != -1:
+			bayes_classifier = GaussianNB(priors=[self.prior, 1 - self.prior])
+		else:
+			bayes_classifier = GaussianNB()
 		bayes_classifier.fit(x, y)
 		self.bayes_model = bayes_classifier
 
 	def predict(self, rs: RecordSet) -> np.ndarray:
 		"""
-		Assigns a predicted class label to the given record sets.
+        Makes the base learner predict validation or testing data.
 
-		:param rs: The record set to assign predictions to.
-		:return: A column vector of predictions corresponding to the record set's rows.
-		"""
+        Two notes need to be made. First, the last column of the provided record set
+        contains the ground truth information. These entries, of course, may not be
+        used during prediction. Second, the output Numpy array needs to be a column
+        vector of two-dimensions: the first (rows) is equal to the length of the number
+        of records, and the second (columns) is equal to 1.
+
+        :param rs: The record set to provide as validation or testing data.
+        :return: The predictions. A column vector with as many rows as the record set has.
+        """
 		predictions: np.ndarray = np.zeros((rs.entries.shape[0], 1))
+		"""
+        print("class prior", self.bayes_model.class_prior_)
+        print("classes", self.bayes_model.classes_)
+        print("variance", self.bayes_model.sigma_)
+        print("mean", self.bayes_model.theta_)
+        """
 		for r in range(rs.entries.shape[0]):
 			predictions[[r], :] = self.bayes_model.predict(rs.entries[[r], :-1])
+		# print(predictions[[r], :])
 		return predictions
+
+
--- main.py
+++ main.py
@@ -120,11 +120,11 @@ if __name__ == '__main__':
 	# learning algorithms and their parameter grids
 	model_grids: Tuple[LearnerParameters, ...] = \
 		cast(Tuple[LearnerParameters, ...], (
-				(KMeansClustering, (('k', (1, 2, 3, 4)),)),
+				(KMeansClustering, (('k', (1, 2, 3)),)),
 				(KNN, (('k', (1, 2, 3)),)),
 				(LinearRegression, (('alpha', (0, 0.009, 0.01)),)),
 				(LogisticRegression, (('alpha', (0.1, 3.5, 5)),)),
-				(NaiveBayes, ()),
+				(NaiveBayes, (('prior', (0.01, 0.5, 0.99, -1)),)),
 				# (NeuralNetwork, ())  # Winward: Set neural network grid search parameters.
 				(ProbitRegression, (('alpha', (0.1, 3.5, 5)),)),
 				(RandomForest, (('depth', (2, 5, 3)),)),
